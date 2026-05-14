346bb8981460c4406e3c48039ba44fdf93f4fe7e
Элина Кокоева <e.kokoeva06@mail.ru>
2026-05-14 19:15:09 +0300

    добавила классы user, post

---
+++ blog/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿namespace blog.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public string AutorId { get; set; }
+
+        public Post(string description, string imagePath, string autorId)
+        {
+            Description = description;
+            ImagePath = imagePath;
+            AutorId = autorId;
+            Id = new Guid();
+        }
+    }
+}
